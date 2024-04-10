41dc48a044616dab766415c38629b14c47fbd97a
Muhammet Taha Adıyaman <68828015+MuhammetTahaAdiyaman@users.noreply.github.com>
2024-04-10 04:02:46 +0300

    Add files via upload

--- oopIntro/src/Main.java
+++ oopIntro/src/Main.java
@@ -2,39 +2,28 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Product product1 = new Product(1,"Lenovo V14",15000,"16 GB Ram"); //referans oluşturma, instance oluşturma
+        Product product1 = new Product(1,"Lenovo V14",15000,"16 GB Ram",0); //referans oluşturma, instance oluşturma
 
 
         Product product2 = new Product(); //referans oluşturma, instance oluşturma
-        product2.id = 2;
-        product2.name = "Lenovo V15";
-        product2.unitPrice = 17000;
-        product2.detail = "8 GB Ram";
+        product2.setId(2);
+        product2.setName("Lenovo V15");
+        product2.setUnitPrice(16000);
+        product2.setDetail("16 GB Ram");
+        product2.setDiscount(10);
 
-        Product product3 = new Product(); //referans oluşturma, instance oluşturma
-        product3.id = 3;
-        product3.name = "Hp Omen 15";
-        product3.unitPrice = 16000;
-        product3.detail = "24 GB Ram";
-
-        Product[] products = {product1,product2,product3};
-
-        for(Product product:products){
-            System.out.println(product.name);
-        }
-        System.out.println(products.length);
+        System.out.println(product1.getUnitPriceAfterDiscount());
 
         Category category1 = new Category();
-        category1.id = 1;
-        category1.name = "Bilgisayar";
+        category1.setId(1);
+        category1.setName("Yiyecek");
 
         Category category2 = new Category();
-        category2.id = 2;
-        category2.name = "Ev/Bahçe";
+        category2.setId(2);
+        category2.setName("İçecek");
 
-        ProductManager productManager = new ProductManager();
-        productManager.addToCard(product1);
-        productManager.addToCard(product2);
-        productManager.addToCard(product3);
+        System.out.println(category1.getName());
+        System.out.println(category2.getName());
+
     }
 }
--- oopIntro/src/Product.java
+++ oopIntro/src/Product.java
@@ -1,21 +1,68 @@
 //burada bir şablon oluşturuyoruz.
 public class Product {
-
+    private int id;
+    private String name;
+    private double unitPrice;
+    private String detail;
+    private double discount;
+    private double unitPriceAfterDiscount;
+    //private sadece bu class içinde kullanıma açık yani erişim sağlayabiliriz anlamına gelmektedir. Bundan dolayı biz getter ve setter methodları oluşturduk.
+    //final sadece constructor da set edilebilir anlamına gelmektedir.
     //bu blokun adı constructor. newlediğimiz anda çalışmasını istediğimiz şeyler burada yazılır.
     public Product(){
-        System.out.println("Ben çalıştım");
-    }
 
-    public Product(int id,String name,double unitPrice, String detail){
+    }
+    public Product(int id,String name,double unitPrice, String detail, double  discount){
         this(); //parametresiz constructor çalıştır demektir.
         this.id = id;
         this.name = name;
         this.unitPrice = unitPrice;
         this.detail = detail;
+        this.discount = discount;
     }
-    int id;
-    String name;
-    double unitPrice;
-    String detail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getUnitPriceAfterDiscount() {
+        return this.unitPrice-(this.unitPrice*this.discount/100);
+    }
 
 }
